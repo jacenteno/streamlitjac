06db3585f097739b4bfc7aedcdc096fa82d6cecd
Jorge Centeno <jacenteno@gmail.com>
2024-05-19 13:19:56 -0500

    actualizacion del uso de extras.stoggle

--- jac_app.py
+++ jac_app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from streamlit_extras.stoggle import stoggle
 
 
 st.header('Bienvenidos  al Mundo  🌎 de StreamLit, con el poder de Python')
 if st.button('Globos?'):
     st.balloons()
 
-
+stoggle(
+    "Click me!",
+    """🥷 Surprise! Here's some additional content""",
+)
 # Título de la aplicación
 st.title('Gráficos con Plotly y Streamlit')
 
@@ -20,20 +24,9 @@ df = pd.read_csv(file_path, delimiter=':', header=None)
 # Suponiendo que conocemos las columnas, podemos definirlas
 df.columns = ["ID", "Description", "Code1", "Code2", "Opera"]
 
-# Verificar los datos de la columna "Opera"
-#st.write("Datos originales de la columna 'Opera':")
-#st.write(df['Opera'])
-
 # Separar el último campo "Opera" en las partes especificadas
 pattern = r'(\d{2})([+-]\d{6})([+-]\d{7})([+-]\d{10})'
 extracted_data = df['Opera'].str.extract(pattern)
-
-# Verificar la forma de extracted_data y las columnas a las que se asignarán
-#st.write("Forma de extracted_data:", extracted_data.shape)
-#st.write("Columnas a asignar:", ['Cod1', 'Signo1', 'Cod2', 'Signo2', 'Cod3', 'Signo3', 'Cod4'])
-
-# Asignar las columnas extraídas al DataFrame df
-#df[['Cod1', 'Signo1', 'Cod2', 'Signo2', 'Cod3', 'Signo3', 'Cod4']] = extracted_data
 
 # Eliminar espacios en blanco alrededor de las descripciones
 df['Description'] = df['Description'].str.strip()
@@ -45,21 +38,13 @@ exclude_id_range = range(1, 900)  # El rango va de 852 a 890 inclusive
 
 df = df[~df['Description'].isin(exclude_descriptions) & ~df['ID'].isin(exclude_id_range)]
 
-
-
-
 # Procesar la columna "Opera" para obtener los últimos 11 caracteres, manejar el signo y dividir entre 100
 df['Opera'] = df['Opera'].str[-11:]  # Tomar los últimos 11 caracteres
 df['Opera'] = df['Opera'].apply(lambda x: float(x) / 100)  # Convertir a float y dividir entre 100
 
-
-
 # Seleccionar columnas para el gráfico en el sidebar
 x_axis = st.sidebar.selectbox("Selecciona la columna para el eje X", df.columns, index=df.columns.get_loc('Description'))
 y_axis = st.sidebar.selectbox("Selecciona la columna para el eje Y", df.columns, index=df.columns.get_loc('Opera'))
-
-
-
 
 # Crear un gráfico de dispersión interactivo con Plotly Express
 fig = px.scatter(df, x=x_axis, y=y_axis)
